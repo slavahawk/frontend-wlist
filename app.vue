8301f8912f24edd2b78406dc6602199d48a8243f
Slava <krylov_s10@mail.ru>
2024-12-13 15:16:18 +0700

    desc test

--- app.vue
+++ app.vue
@@ -2,4 +2,11 @@
   <NuxtPage />
 </template>
 <script setup lang="ts">
+
+useSeoMeta({
+  title: 'W-List - Винная карта',
+  ogTitle: 'W-List - Винная карта',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 
 import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
+
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
