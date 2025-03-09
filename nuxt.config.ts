c6051725f350113f399da15a6cfe8688d89bdb2e
Slava <krylov_s10@mail.ru>
2025-03-10 06:57:05 +0700

    favicon fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,4 +23,50 @@ export default defineNuxtConfig({
   plugins: [
     "~/plugins/yandex-metrika.ts", // добавьте путь к вашему плагину
   ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon", // Или "image/ico"
+          href: "/favicon.ico", // Укажите путь к вашему favicon
+        },
+        // 16x16 иконка для браузеров
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        // 32x32 иконка для браузеров
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        // Иконка для Apple устройства
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-icon.png",
+        },
+        // Иконка для Android
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "192x192",
+          href: "/android-chrome-192x192.png",
+        },
+        // Иконка для старых устройств и браузеров
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "512x512",
+          href: "/android-chrome-512x512.png",
+        },
+        // Поместите другие размеры иконок, если необходимо
+      ],
+    },
+  },
 });
